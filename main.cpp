258f0353fa89ada5bc4215f15e1a7081b9381730
Hezio Cumba <heziocumba10@gmail.com>
2023-08-23 20:34:42 -0300

    SG de um Parque de Estacionamento

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <stdlib.h>
+#include "CLASSES.HPP"
+#include "VEICULO.HPP"
+#include "VAGAS.HPP" 
+#include "TICKET.HPP"
+#include "ESTACIONAMENTO.HPP"
+#include "START.HPP"
+
+    int main() {
+            start();
+        return 0;
+    } 
+
+/*
+======================================= AUTORES =======================================
+    Albertina Moisés Muianga
+    Hézio Castigo Cumba
+    Wady Paulino Rogério Munguambe
+======================================= AUTORES =======================================
+*/
